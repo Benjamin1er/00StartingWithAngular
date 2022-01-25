4a65d251851d3181ce0e8024c2a3c304f94999e2
Benjamin Geron <benjamin1er@MacBook-Pro-de-Benjamin.local>
2022-01-25 15:56:20 +0100

    Changed title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'firstApp';
+  title = 'Bienvenue sur le site de Benjamin 1er';
 }
